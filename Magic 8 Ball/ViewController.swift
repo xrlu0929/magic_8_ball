6c9d52dba15e519a3f931d59f66886646746abda
xrlu0929 <xl2474@tc.columbia.edu>
2018-07-19 23:25:12 -0400

    ball compeletion

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageList = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomIndex: Int = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "ball1")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        randomImage ()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomImage()
+    }
+    func randomImage () {
+        randomIndex = Int(arc4random_uniform(6))
+        imageView.image = UIImage(named: imageList[randomIndex])
+    }
 }
 
